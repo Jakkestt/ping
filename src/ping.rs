6bd8e1e6c10e98c7444c6892a07e7ae1f67a3d3a
Jarkko Arffman <41551796+Jakkestt@users.noreply.github.com>
2022-06-17 03:17:22 +0300

    Fix some mistakes like not checking both icmp type and code and ipv6 sockets not having a method called set_ttl

--- src/ping.rs
+++ src/ping.rs
@@ -1,18 +1,25 @@
-use std::net::{SocketAddr, IpAddr};
 use std::io::Read;
+use std::net::{IpAddr, SocketAddr};
 use std::time::Duration;
 
 use rand::random;
 use socket2::{Domain, Protocol, Socket, Type};
 
-use crate::errors::{Error};
-use crate::packet::{EchoReply, EchoRequest, IpV4Packet, IcmpV4, IcmpV6, ICMP_HEADER_SIZE};
+use crate::errors::Error;
+use crate::packet::{EchoReply, EchoRequest, IcmpV4, IcmpV6, IpV4Packet, ICMP_HEADER_SIZE};
 
 const TOKEN_SIZE: usize = 24;
 const ECHO_REQUEST_BUFFER_SIZE: usize = ICMP_HEADER_SIZE + TOKEN_SIZE;
 type Token = [u8; TOKEN_SIZE];
 
-pub fn ping(addr: IpAddr, timeout: Option<Duration>, ttl: Option<u32>, ident: Option<u16>, seq_cnt: Option<u16>, payload: Option<&Token>) -> Result<(), Error> {
+pub fn ping(
+    addr: IpAddr,
+    timeout: Option<Duration>,
+    ttl: Option<u32>,
+    ident: Option<u16>,
+    seq_cnt: Option<u16>,
+    payload: Option<&Token>,
+) -> Result<(), Error> {
     let timeout = match timeout {
         Some(timeout) => Some(timeout),
         None => Some(Duration::from_secs(4)),
@@ -41,7 +48,11 @@ pub fn ping(addr: IpAddr, timeout: Option<Duration>, ttl: Option<u32>, ident: Op
         Socket::new(Domain::IPV6, Type::RAW, Some(Protocol::ICMPV6))?
     };
 
-    socket.set_ttl(ttl.unwrap_or(64))?;
+    if dest.is_ipv4() {
+        socket.set_ttl(ttl.unwrap_or(64))?;
+    } else {
+        socket.set_unicast_hops_v6(ttl.unwrap_or(64))?;
+    }
 
     socket.set_write_timeout(timeout)?;
 
